4de331ac2f9f213921e010afb0578341195f8f38
Kelsey-S-G <144941141+Kelsey-S-G@users.noreply.github.com>
2024-12-18 21:28:40 +0000

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const handleLogin = async (event) => {
   }
 
   try {
-    const response = await fetch("/api/auth/login", {", {
+    const response = await fetch("/api/auth/login", {
       method: "POST",
       headers: { 
         "Content-Type": "application/json",
